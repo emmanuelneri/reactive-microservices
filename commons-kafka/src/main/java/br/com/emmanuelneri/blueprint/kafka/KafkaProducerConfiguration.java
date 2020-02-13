42b2800628220f356dfd3ea693be5a7b1ab98185
emmanuelneri <emmanuelnerisouza@gmail.com>
2020-02-12 23:36:43 -0300

    creating kafka commons

---
+++ commons-kafka/src/main/java/br/com/emmanuelneri/blueprint/kafka/KafkaProducerConfiguration.java
@@ -0,0 +1,26 @@
+package br.com.emmanuelneri.blueprint.kafka;
+
+import io.vertx.core.json.JsonObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public final class KafkaProducerConfiguration extends KafkaConfiguration {
+
+    protected final String keySerializer;
+    protected final String valueSerializer;
+
+    public KafkaProducerConfiguration(final JsonObject configuration) {
+        super(configuration);
+        this.keySerializer = configuration.getString("kafka.key.serializer");
+        this.valueSerializer = configuration.getString("kafka.value.serializer");
+    }
+
+    public Map<String, String> createConfig() {
+        final Map<String, String> config = new HashMap<>();
+        config.put("bootstrap.servers", this.bootstrapServers);
+        config.put("key.serializer", this.keySerializer);
+        config.put("value.serializer", this.valueSerializer);
+        return config;
+    }
+}
