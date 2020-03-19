62be773a042b5efb13e0b5e0455cc953c04be902
emmanuelneri <emmanuelnerisouza@gmail.com>
2020-03-18 23:05:27 -0300

    fixings test

--- commons-config/src/test/java/br/com/emmanuelneri/blueprint/commons/config/ConfigRetrieverConfigurationTest.java
+++ commons-config/src/test/java/br/com/emmanuelneri/blueprint/commons/config/ConfigRetrieverConfigurationTest.java
@@ -13,7 +13,7 @@ public class ConfigRetrieverConfigurationTest {
     public void wrongApplicationNameShouldFailTest(final TestContext context) {
         final Vertx vertx = Vertx.vertx();
 
-        ConfigRetrieverConfiguration.configure(vertx, "test")
+        ConfigRetrieverConfiguration.configure(vertx, "commons-config")
                 .getConfig(configurationHandler -> context.asyncAssertFailure());
     }
 
