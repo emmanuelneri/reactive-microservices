5750b28bb81bff9b7e409d1f205e9c173728cec5
emmanuelneri <emmanuelnerisouza@gmail.com>
2020-04-15 22:34:02 -0300

    fixing validation

--- schedule-command/src/main/java/br/com/emmanuelneri/reactivemicroservices/schedule/command/interfaces/SchedulePersistenceVerticle.java
+++ schedule-command/src/main/java/br/com/emmanuelneri/reactivemicroservices/schedule/command/interfaces/SchedulePersistenceVerticle.java
@@ -7,17 +7,24 @@ import br.com.emmanuelneri.reactivemicroservices.schedule.command.domain.Schedul
 import com.datastax.driver.core.BoundStatement;
 import com.datastax.driver.core.PreparedStatement;
 import io.vertx.cassandra.CassandraClient;
+import io.vertx.cassandra.ResultSet;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.eventbus.Message;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 import lombok.AllArgsConstructor;
 
 import static br.com.emmanuelneri.reactivemicroservices.vertx.eventbus.MessageError.CONNECTION_ERROR;
 
 @AllArgsConstructor
 public class SchedulePersistenceVerticle extends AbstractVerticle {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AbstractVerticle.class);
+    private static final String REPLY_OK = "ok";
 
     private final CassandraConfiguration cassandraConfiguration;
 
@@ -29,47 +36,34 @@ public class SchedulePersistenceVerticle extends AbstractVerticle {
 
     private Handler<Message<JsonObject>> persist(final CassandraClient client) {
         return message -> {
-            System.out.println("----- INIT ---------------");
             final Schedule schedule = message.body().mapTo(Schedule.class);
-            System.out.println("----- Convert ---------------");
-
             client.prepare("INSERT INTO schedule (data_time, description, document_number, customer, phone, email) VALUES (?,?,?,?,?,?)", prepareResultHandler -> {
-
-                System.out.println("----- PREPARED ---------------");
-
-
                 if (prepareResultHandler.failed()) {
+                    LOGGER.error("prepareStatement error", prepareResultHandler.cause());
                     message.fail(CONNECTION_ERROR.getCode(), prepareResultHandler.cause().getMessage());
                     return;
                 }
 
-                try {
-                    final PreparedStatement preparedStatement = prepareResultHandler.result();
-                    preparedStatement.getCodecRegistry().register(LocalDateTimeCodec.instance);
-                    final BoundStatement boundStatement = preparedStatement
-                            .bind(schedule.getDateTime(), schedule.getDescription(), schedule.getDocumentNumber(),
-                                    schedule.getCustomer(), schedule.getPhone(), schedule.getEmail());
+                final PreparedStatement preparedStatement = prepareResultHandler.result();
+                preparedStatement.getCodecRegistry().register(LocalDateTimeCodec.instance);
+                final BoundStatement boundStatement = preparedStatement
+                        .bind(schedule.getDateTime(), schedule.getDescription(), schedule.getDocumentNumber(),
+                                schedule.getCustomer(), schedule.getPhone(), schedule.getEmail());
 
-                    client.execute(boundStatement, executeResultHandler -> {
-                        System.out.println("----- executed ---------------");
-
-
-                        if (prepareResultHandler.failed()) {
-                            message.fail(CONNECTION_ERROR.getCode(), executeResultHandler.cause().getMessage());
-                            return;
-                        }
-
-                        System.out.println("----- REPLY ---------------");
-
-                        message.reply("ok");
-                    });
-
-                } catch (Exception ex) {
-                    System.out.println("----- ERRRo ---------------");
-
-                    message.fail(CONNECTION_ERROR.getCode(), ex.getMessage());
-                }
+                client.execute(boundStatement, handlerExecute(message));
             });
+        };
+    }
+
+    private Handler<AsyncResult<ResultSet>> handlerExecute(final Message<JsonObject> message) {
+        return executeResultHandler -> {
+            if (executeResultHandler.failed()) {
+                LOGGER.error("execute error", executeResultHandler.cause());
+                message.fail(CONNECTION_ERROR.getCode(), executeResultHandler.cause().getMessage());
+                return;
+            }
+
+            message.reply(REPLY_OK);
         };
     }
 }
