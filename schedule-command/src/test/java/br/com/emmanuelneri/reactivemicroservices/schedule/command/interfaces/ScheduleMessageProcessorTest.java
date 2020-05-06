87fefd963e8f658326d80eb37744bd3c4e1cab16
emmanuelneri <emmanuelnerisouza@gmail.com>
2020-05-05 23:40:42 -0300

    change test

--- schedule-command/src/test/java/br/com/emmanuelneri/reactivemicroservices/schedule/command/interfaces/ScheduleMessageProcessorTest.java
+++ schedule-command/src/test/java/br/com/emmanuelneri/reactivemicroservices/schedule/command/interfaces/ScheduleMessageProcessorTest.java
@@ -2,6 +2,7 @@ package br.com.emmanuelneri.reactivemicroservices.schedule.command.interfaces;
 
 import br.com.emmanuelneri.reactivemicroservices.errors.InvalidMessage;
 import br.com.emmanuelneri.reactivemicroservices.errors.InvalidMessageReason;
+import br.com.emmanuelneri.reactivemicroservices.schedule.schema.CustomerSchema;
 import br.com.emmanuelneri.reactivemicroservices.schedule.schema.RequestResult;
 import br.com.emmanuelneri.reactivemicroservices.schedule.schema.ScheduleSchema;
 import br.com.emmanuelneri.reactivemicroservices.vertx.core.VertxBuilder;
@@ -17,6 +18,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import static br.com.emmanuelneri.reactivemicroservices.schedule.command.interfaces.ScheduleMessageProcessor.INVALID_SCHEDULE_RECEIVED_ADDRESS;
@@ -44,19 +46,27 @@ public class ScheduleMessageProcessorTest {
 
         final String requestId = UUID.randomUUID().toString();
         final Async async = context.async();
-        this.vertx.eventBus().consumer(SCHEDULE_RETURN_REQUEST_PROCESSED_ADDRESS, messageResult -> {
-            final RequestResult requestResult = Json.decodeValue(messageResult.body().toString(), RequestResult.class);
+        this.vertx.eventBus().<String>consumer(SCHEDULE_RETURN_REQUEST_PROCESSED_ADDRESS, messageResult -> {
+            final RequestResult requestResult = Json.decodeValue(messageResult.body(), RequestResult.class);
             if (requestId.equals(requestResult.getRequestId())) {
                 context.assertTrue(requestResult.isSuccess(), requestResult.toString());
                 async.complete();
             }
         });
 
-        final ScheduleMessageProcessor scheduleMessageProcessor = ScheduleMessageProcessor.create(this.vertx);
-        final String messageValue = "{\"dateTime\":\"2020-05-09T12:14:50.786\",\"customer\":{\"name\":\"Customer 1\",\"documentNumber\":948948393849,\"phone\":\"4499099493\"},\"description\":\"Success Test\"}";
+        final CustomerSchema customerSchema = new CustomerSchema();
+        customerSchema.setDocumentNumber("948948393849");
+        customerSchema.setName("Customer 1");
+        customerSchema.setPhone("4499099493");
 
+        final ScheduleSchema schema = new ScheduleSchema();
+        schema.setCustomer(customerSchema);
+        schema.setDateTime(LocalDateTime.now().plusDays(1));
+        schema.setDescription("Complete Test");
+
+        final ScheduleMessageProcessor scheduleMessageProcessor = ScheduleMessageProcessor.create(this.vertx);
         final Promise<Void> promise = Promise.promise();
-        final ConsumerRecord<String, String> record = new ConsumerRecord<>("success.topic", 0, 0, "1", messageValue);
+        final ConsumerRecord<String, String> record = new ConsumerRecord<>("success.topic", 0, 0, "1", Json.encode(schema));
         record.headers().add(ScheduleSchema.REQUEST_ID_HEADER, requestId.getBytes());
         scheduleMessageProcessor.process(record, promise);
         promise.future().setHandler(resultHandler -> {
