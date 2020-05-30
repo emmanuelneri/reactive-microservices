fda6604a5f979d69e360e6c8109cd9c6a932cb89
emmanuelneri <emmanuelnerisouza@gmail.com>
2020-05-30 14:10:09 -0300

    producing message in order test

--- schedule-connector/src/test/java/br/com/emmanuelneri/reactivemicroservices/schedule/connector/interfaces/ScheduleConnectorIT.java
+++ schedule-connector/src/test/java/br/com/emmanuelneri/reactivemicroservices/schedule/connector/interfaces/ScheduleConnectorIT.java
@@ -13,6 +13,8 @@ import io.vertx.core.buffer.Buffer;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
@@ -21,6 +23,10 @@ import io.vertx.ext.web.client.HttpResponse;
 import io.vertx.ext.web.client.WebClient;
 import io.vertx.kafka.client.consumer.KafkaConsumer;
 import io.vertx.kafka.client.producer.KafkaHeader;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -28,12 +34,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.testcontainers.containers.KafkaContainer;
 
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Map;
-
 @RunWith(VertxUnitRunner.class)
 public class ScheduleConnectorIT {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ScheduleConnectorIT.class);
 
     private static final int PORT = 8888;
     private static final String HOST = "localhost";
@@ -48,13 +52,13 @@ public class ScheduleConnectorIT {
     @Before
     public void before() {
         configuration = new JsonObject()
-                .put("kafka.bootstrap.servers", kafka.getBootstrapServers())
-                .put("kafka.key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
-                .put("kafka.value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
-                .put("kafka.key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
-                .put("kafka.value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
-                .put("kafka.offset.reset", "earliest")
-                .put("kafka.enable.auto.commit", false);
+            .put("kafka.bootstrap.servers", kafka.getBootstrapServers())
+            .put("kafka.key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+            .put("kafka.value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+            .put("kafka.key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+            .put("kafka.value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+            .put("kafka.offset.reset", "earliest")
+            .put("kafka.enable.auto.commit", false);
 
         this.vertx = VertxBuilder.createAndConfigure();
     }
@@ -92,40 +96,116 @@ public class ScheduleConnectorIT {
 
         final Async async = context.async();
         httpServer.requestHandler(router)
-                .listen(PORT, serverAsyncResult -> {
-                    if (serverAsyncResult.failed()) {
-                        context.fail(serverAsyncResult.cause());
-                    }
+            .listen(PORT, serverAsyncResult -> {
+                if (serverAsyncResult.failed()) {
+                    context.fail(serverAsyncResult.cause());
+                }
 
-                    client.post(PORT, HOST, URI)
+                client.post(PORT, HOST, URI)
+                    .sendJson(schedule, clientAsyncResult -> {
+                        if (clientAsyncResult.failed()) {
+                            context.fail(clientAsyncResult.cause());
+                        }
+
+                        final HttpResponse<Buffer> result = clientAsyncResult.result();
+                        context.assertEquals(201, result.statusCode());
+                        context.assertEquals(String.format("{\"status\":\"OK\",\"message\":\"%s\"}", schedule.getRequestId()), result.bodyAsString());
+
+                        kafkaConsumer.handler(consumerRecord -> {
+                            context.assertNotNull(consumerRecord.key());
+                            context.assertEquals("948948393849", consumerRecord.key());
+
+                            final ScheduleSchema consumedSchema = Json.decodeValue(consumerRecord.value(), ScheduleSchema.class);
+                            context.assertEquals(schedule.getDescription(), consumedSchema.getDescription());
+                            context.assertEquals(schedule.getDateTime(), consumedSchema.getDateTime());
+                            context.assertEquals(schedule.getCustomer().getDocumentNumber(), consumedSchema.getCustomer().getDocumentNumber());
+                            context.assertEquals(schedule.getCustomer().getName(), consumedSchema.getCustomer().getName());
+
+                            final List<KafkaHeader> headers = consumerRecord.headers();
+                            context.assertNotNull(headers.get(0));
+                            context.assertEquals(ScheduleSchema.REQUEST_ID_HEADER, headers.get(0).key());
+                            context.assertEquals(schedule.getRequestId().toString(), headers.get(0).value().toString());
+
+                            httpServer.close();
+                            async.complete();
+                        });
+                    });
+            });
+    }
+
+    @Test
+    public void shouldProduceMensageInOrder(final TestContext context) {
+        final KafkaProducerConfiguration kafkaProducerConfiguration = new KafkaProducerConfiguration(configuration);
+        final Router router = Router.router(vertx);
+
+        this.vertx.deployVerticle(new ScheduleRequestProcessor());
+        this.vertx.deployVerticle(new ScheduleProducer(kafkaProducerConfiguration));
+        this.vertx.deployVerticle(new ScheduleRequestEndpoint((router)));
+
+        final Map<String, String> kafkaConsumerConfiguration = new KafkaConsumerConfiguration(configuration).createConfig("order-test-schedule-consumer");
+        final KafkaConsumer<String, String> kafkaConsumer = KafkaConsumer.create(this.vertx, kafkaConsumerConfiguration);
+        kafkaConsumer.subscribe(ScheduleProducer.SCHEDULE_REQUEST_TOPIC);
+
+        final WebClient client = WebClient.create(this.vertx);
+        final HttpServer httpServer = this.vertx.createHttpServer();
+
+        final Async async = context.async();
+        httpServer.requestHandler(router)
+            .listen(PORT, serverAsyncResult -> {
+                if (serverAsyncResult.failed()) {
+                    context.fail(serverAsyncResult.cause());
+                }
+
+                final int count = 20;
+                for (int i = 0; i < count; i++) {
+                    final String description = "schedule:" + i;
+                    final Schedule schedule = createSchedule(description);
+
+                    vertx.executeBlocking(handler -> {
+                        client.post(PORT, HOST, URI)
                             .sendJson(schedule, clientAsyncResult -> {
                                 if (clientAsyncResult.failed()) {
                                     context.fail(clientAsyncResult.cause());
                                 }
 
-                                final HttpResponse<Buffer> result = clientAsyncResult.result();
-                                context.assertEquals(201, result.statusCode());
-                                context.assertEquals(String.format("{\"status\":\"OK\",\"message\":\"%s\"}", schedule.getRequestId()), result.bodyAsString());
-
-                                kafkaConsumer.handler(consumerRecord -> {
-                                    context.assertNotNull(consumerRecord.key());
-                                    context.assertEquals("948948393849", consumerRecord.key());
-
-                                    final ScheduleSchema consumedSchema = Json.decodeValue(consumerRecord.value(), ScheduleSchema.class);
-                                    context.assertEquals(schedule.getDescription(), consumedSchema.getDescription());
-                                    context.assertEquals(schedule.getDateTime(), consumedSchema.getDateTime());
-                                    context.assertEquals(schedule.getCustomer().getDocumentNumber(), consumedSchema.getCustomer().getDocumentNumber());
-                                    context.assertEquals(schedule.getCustomer().getName(), consumedSchema.getCustomer().getName());
-
-                                    final List<KafkaHeader> headers = consumerRecord.headers();
-                                    context.assertNotNull(headers.get(0));
-                                    context.assertEquals(ScheduleSchema.REQUEST_ID_HEADER, headers.get(0).key());
-                                    context.assertEquals(schedule.getRequestId().toString(), headers.get(0).value().toString());
-
-                                    httpServer.close();
-                                    async.complete();
-                                });
+                                context.assertEquals(201, clientAsyncResult.result().statusCode());
+                                LOGGER.info("{0} sended", description);
                             });
+                    }, result -> {
+                        if (result.failed()) {
+                            context.fail(result.cause());
+                        }
+                    });
+                }
+
+                final AtomicInteger consumedCount = new AtomicInteger();
+                kafkaConsumer.handler(consumerRecord -> {
+                    final int order = consumedCount.getAndIncrement();
+
+                    final ScheduleSchema consumedSchema = Json.decodeValue(consumerRecord.value(), ScheduleSchema.class);
+                    context.assertEquals("schedule:" + order, consumedSchema.getDescription());
+
+                    LOGGER.info("consumed description: {0} - offset: {1}", consumedSchema.getDescription(), consumerRecord.offset());
+
+                    if (consumedCount.get() == count) {
+                        httpServer.close();
+                        async.complete();
+                    }
                 });
+            });
+    }
+
+    private Schedule createSchedule(final String description) {
+        final Customer customerSchema = new Customer();
+        customerSchema.setDocumentNumber("948948393849");
+        customerSchema.setName("Customer 1");
+        customerSchema.setPhone("4499099493");
+
+        final Schedule schedule = new Schedule();
+        schedule.setCustomer(customerSchema);
+        schedule.setDateTime(LocalDateTime.now().plusDays(1));
+        schedule.setDescription(description);
+
+        return schedule;
     }
 }
